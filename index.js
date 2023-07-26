2b97de061cacf7135b54c20b97e4c87de06d5f3f
Alex Miserandino <amiserandino20@gmail.com>
2023-07-26 13:58:50 -0700

    added requests

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// index.js
-// where your node app starts
 
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,15 +15,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// valid date returns json object w/unix + utc key
+app.get("/api/:date", (req, res) => {
+  const seconds = req.params.date
+  const date = new Date(req.params.date)
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+if (date != "Invalid Date") {
+  res.json({ unix: date.getTime(), utc: date.toUTCString() })
+} else if (!isNaN(seconds)) {
+  return res.json({ unix: parseInt(seconds), utc: new Date(parseInt(seconds)).toUTCString() })
+} else {
+  res.json({ error: "Invalid Date" })
+}
+})
+
+// empty date parameter returns current time in JSON object
+app.get('/api', (req, res) => {
+  res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() })
+})
+
+ const listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
 });
 
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
